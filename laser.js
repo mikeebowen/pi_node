e61cb1cf259c3bcb3635a9deaaec8adbf753a5a9
Michael Bowen <mikee.bowen@gmail.com>
2019-01-05 13:09:53 -0800

    call .readSync as function

--- laser.js
+++ laser.js
@@ -1,17 +1,17 @@
 'use strict';
 const Gpio = require('onoff').Gpio;
 const laser = new Gpio(4, 'out');
-const interval = setInterval(blinkLaser, 500);
+const lasterInterval = setInterval(blinkLaser, 500);
 
 function blinkLaser() {
-  if (laser.readSync === 0) {
+  if (laser.readSync() === 0) {
     laser.writeSync(1);
   } else {
     laser.writeSync(0);
   }
 
   function endBlink() {
-    clearInterval(interval);
+    clearInterval(lasterInterval);
     laser.writeSync(0);
     laser.unexport();
   }
