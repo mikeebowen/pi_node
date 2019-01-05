4dcf39096cb825cf27b1d15a5da1f4b4682c187a
Michael Bowen <mikee.bowen@gmail.com>
2019-01-05 13:15:18 -0800

    change interval to 250

--- laser.js
+++ laser.js
@@ -1,7 +1,7 @@
 'use strict';
 const Gpio = require('onoff').Gpio;
 const laser = new Gpio(4, 'out');
-const lasterInterval = setInterval(blinkLaser, 500);
+const lasterInterval = setInterval(blinkLaser, 250);
 
 function blinkLaser() {
   if (laser.readSync() === 0) {
