96a38580686b9eb0a0a976bdaab444890f45664c
Michael Bowen <mikee.bowen@gmail.com>
2019-01-06 12:30:43 -0800

    add function to show alphabet on 7 segment display

--- 7segmentDisplay.js
+++ 7segmentDisplay.js
@@ -2,16 +2,45 @@
 
 const Gpio = require('onoff').Gpio;
 
-const e = new Gpio(17, 'out');
-const d = new Gpio(16, 'out');
-const c = new Gpio(13, 'out');
-const dp = new Gpio(12, 'out');
-const g = new Gpio(18, 'out');
-const f = new Gpio(19, 'out');
-const a = new Gpio(20, 'out');
-const b = new Gpio(21, 'out');
+const top = new Gpio(20, 'out');
+const tr = new Gpio(21, 'out');
+const br = new Gpio(13, 'out');
+const btm = new Gpio(16, 'out');
+const bl = new Gpio(17, 'out');
+const tl = new Gpio(19, 'out');
+const ctr = new Gpio(18, 'out');
+const dot = new Gpio(12, 'out');
 
-const lights = [e, d, c, dp, g, f, a, b];
+const lights = [top, tr, br, btm, bl, tl, ctr, dot];
+
+const letters = {
+  a: [1, 1, 1, 0, 1, 1, 1, 0],
+  b: [0, 0, 1, 1, 1, 1, 1, 0],
+  c: [1, 0, 0, 1, 1, 1, 0, 0],
+  d: [0, 1, 1, 1, 1, 0, 1, 0],
+  e: [1, 1, 1, 1, 0, 0, 1, 0],
+  f: [1, 0, 0, 0, 1, 1, 1, 0],
+  g: [1, 1, 1, 1, 0, 1, 1, 0],
+  h: [0, 1, 1, 0, 1, 1, 1, 0],
+  i: [0, 1, 1, 0, 0, 0, 0, 0],
+  j: [0, 1, 1, 1, 1, 0, 0, 0],
+  k: [0, 1, 0, 1, 1, 1, 0, 0],
+  l: [0, 0, 0, 1, 1, 1, 0, 0],
+  m: [0, 0, 1, 0, 1, 0, 1, 0],
+  n: [1, 1, 1, 0, 1, 1, 0, 0],
+  o: [1, 1, 1, 1, 1, 1, 0, 0],
+  p: [1, 1, 0, 0, 1, 1, 1, 0],
+  q: [1, 1, 1, 0, 0, 1, 1, 0],
+  r: [0, 0, 0, 0, 1, 0, 1, 0],
+  s: [1, 0, 1, 1, 0, 1, 1, 0],
+  t: [0, 0, 0, 1, 1, 1, 1, 0],
+  u: [0, 1, 1, 1, 1, 1, 0, 0],
+  v: [0, 1, 1, 1, 1, 1, 0, 0],
+  w: [0, 0, 1, 1, 1, 0, 0, 0],
+  x: [0, 1, 1, 0, 1, 1, 1, 0],
+  y: [0, 1, 1, 1, 0, 1, 1, 0],
+  z: [1, 0, 1, 1, 0, 1, 1, 0]
+};
 
 // lights.forEach(x => {
 //   x.writeSync(1);
@@ -47,17 +76,40 @@ function cycleLedAsync(i) {
           cycleLedAsync(i);
         }
       });
-    }, 1000);
+    }, 500);
   });
 }
 
-// cycleLed(0);
-cycleLedAsync(0);
+function showLetter(l) {
+  letters[l].forEach((v, i) => {
+    lights[i].write(v, err => {
+      if (err) {
+        throw err;
+      }
+    });
+  });
+}
 
-setTimeout(() => {
+function showAlphabet() {
+  for (let letter in letters) {
+    if (letter === 'a') {
+      showLetter(letter);
+    } else {
+      setTimeout(() => {
+        showLetter(letter);      
+      }, 250);
+    }
+  }
+}
+
+function cleanUp() {
   lights.forEach(x => {
     x.writeSync(0);
     x.unexport();
   });
-}, 30000);
+}
 
+// cycleLed(0);
+Promise.resolve(cycleLedAsync(0))
+  .then(() => showAlphabet())
+  .then(() => cleanUp());
