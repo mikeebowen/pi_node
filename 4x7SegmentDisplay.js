2ba6fcb92fc1d59dd09345852895f6ac97ba8469
Michael Bowen <mikee.bowen@gmail.com>
2019-01-12 12:03:40 -0800

    add initial 4x7SegmentDisplay file

---
+++ 4x7SegmentDisplay.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Gpio = require('onoff').Gpio;
+
+const a = new Gpio(4, 'out');
+const b = new Gpio(17, 'out');
+const c = new Gpio(27, 'out');
+const d = new Gpio(22, 'out');
+const e = new Gpio(5, 'out');
+const f = new Gpio(6, 'out');
+const g = new Gpio(13, 'out');
+const h = new Gpio(18, 'out');
+const i = new Gpio(23, 'out');
+const j = new Gpio(25, 'out');
+const k = new Gpio(12, 'out');
+const l = new Gpio(16, 'out');
+
+const lights = [a, b, c, d, e, f, g, h, i, j, k, l];
+
+function showLight(i) {
+  lights[i].write(1, err => {
+    if (err) {
+      throw err;
+    }
+    i += 1;
+    setTimeout(() => {
+      lights[i].write(0, err => {
+        if (err) {
+          throw err;
+        }
+        if (i < lights.length) {
+          showLight(i);
+        }
+      });
+    }, 5000);
+  });
+}
+
+showLight(0);
