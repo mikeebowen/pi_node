0bf7e56ce52111830b8d577d5ca8c1815067dc49
Michael Bowen <mikee.bowen@gmail.com>
2019-01-15 22:43:04 -0800

    use correct comparison

--- 4x7SegmentDisplay.js
+++ 4x7SegmentDisplay.js
@@ -84,7 +84,7 @@ function showLight(i) {
 
 function writeNumSync(time) {
   letters[time[index]].forEach((bit, ii) => {
-    bit = ii === 1 ? 1 : bit;
+    bit = ii === letters.length - 1 ? 1 : bit;
 		console.log("​writeNumSync -> ii", ii)
 		console.log("​writeNumSync -> bit", bit)
     lights[ii].writeSync(bit);
